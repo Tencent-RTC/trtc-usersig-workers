3439d316251246c5674b68eb2ecf7ef9de3e44c0
leolxliu <leolxliu@tencent.com>
2024-01-10 14:51:11 +0800

    init repo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
 import { Hono } from 'hono'
+import Sig from './usersig'
 
-const app = new Hono()
 
-app.get('/', (c) => c.text('Hello Hono!'))
+type Bindings = {
+    SDKAPPID: number
+    SECRET: string
+}
+
+const app = new Hono<{ Bindings: Bindings}>()
+
+
+app.post('/config', async (c) => {
+
+    console.log(c.req)
+
+    const body = await c.req.json()
+    const userid = body.userid || 'test'
+    const expire = body.expire || 86400
+
+    const userSig = new Sig.Api(c.env.SDKAPPID, c.env.SECRET)
+    
+    const sig = userSig.genUserSig(userid, Number(expire))
+
+    return c.json({
+        sdkappid: c.env.SDKAPPID,
+        userSig: sig,
+    })
+})
+
+
+app.get('/test', (c) => c.text('Hello Hono!'))
 
 export default app
